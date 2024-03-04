26b9a5402e13a30b45f68f47ca61740f6494d911
xyf <2316639449@qq.com>
2024-03-04 19:29:14 +0000

    add some files

---
+++ models/UserBasic.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type UserBasic struct {
+	gorm.Model
+	Name          string
+	PassWord      string
+	Email         string
+	Identity      string
+	ClientIp      string
+	ClientPort    string
+	LoginTime     uint64
+	HeartbeatTime uint64
+	LoginOutTime  uint64
+	IsLogout      bool
+	DeviceInfo    string
+}
+
+func (table *UserBasic) TableName() string {
+	return "user_basic"
+}
